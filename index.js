e81d63669eb8b384c2af9fe35aaaf49046e50a44
Ikhwanul Abiyu <ikhwanulabiyu@gmail.com>
2025-01-30 16:31:45 +0700

    feat: added cors

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
+
+
 const authRoutes = require('./routes/auth.routes');
 require('dotenv').config();
 
 const app = express();
+
+app.use(cors(
+  {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }
+));
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
