11bf22268ed4232787e5880a6060604e62f051bf
Ikhwanul Abiyu <ikhwanulabiyu@gmail.com>
2025-02-02 12:40:16 +0700

    feat: base url change

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors(
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const baseUrl = '/api/v1';
+const baseUrl = '/v1';
 
 app.use(`${baseUrl}/auth`, authRoutes);
 
