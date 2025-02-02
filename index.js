11a6455850e59dda1e92862438f76557a32282f6
Ikhwanul Abiyu <ikhwanulabiyu@gmail.com>
2025-02-02 09:49:29 +0700

    feat: add base url

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
 
-
 const authRoutes = require('./routes/auth.routes');
 require('dotenv').config();
 
@@ -17,12 +16,25 @@ app.use(cors(
 ));
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
+const baseUrl = '/api/v1';
 
-app.use('/api/auth', authRoutes);
+app.use(`${baseUrl}/auth`, authRoutes);
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get('/', (req, res) => {
+  res.json({ message: 'API is Running' });
+});
+
+app.get(baseUrl, (req, res) => {
+  res.json({ message: 'V1 API' });
+});
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log('Connected to the database');
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  });
+}).catch((error) => {
+  console.log('Error:', error);
+});
